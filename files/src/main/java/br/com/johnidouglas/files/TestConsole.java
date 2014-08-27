0f2e1ed1d747e0ae2af73eb8d6ecfeee5dc0e06f
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2014-08-27 17:45:45 -0300

    ad

---
+++ files/src/main/java/br/com/johnidouglas/files/TestConsole.java
@@ -0,0 +1,24 @@
+package br.com.johnidouglas.files;
+
+
+import java.awt.Container;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TestConsole {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+			
+		JButtonAction2 tela = new JButtonAction2();
+		tela.createFrame();
+		
+		
+
+	}
+
+}
