a71647159246e9c306f56060ccde4c8e29a16b7f
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2014-09-29 17:34:05 -0300

    Create 1002.c

---
+++ uri-online-judge/1002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float R; 
+    float A;
+    float PI = 3.14159;
+    
+    scanf("%f", &R);
+    
+    A=(2*R)*PI;
+    printf("A=%.4f\n", A);
+    
+    return 0;
+}
