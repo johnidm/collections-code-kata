092a5838655be0310db09249ad046ae599f911e8
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2015-02-03 17:56:38 -0200

    finalizado o projeto

--- falador/json-parse.sh
+++ falador/json-parse.sh
@@ -1,9 +1,13 @@
-function parse_json()
-{
-    echo $1 | sed -e 's/[{}]/''/g' | awk -F=':' -v RS=',' "\$1~/\"$2\"/ {print}" | sed -e "s/\"$2\"://" | tr -d "\n\t" | sed -e 's/\\"/"/g' | sed -e 's/\\\\/\\/g' | sed -e 's/^[ \t]*//g' | sed -e 's/^"//'  -e 's/"$//'	
-}
+#function parse_json()
+#{
+#    echo $1 | sed -e 's/[{}]/''/g' | awk -F=':' -v RS=',' "\$1~/\"$2\"/ {print}" | sed -e "s/\"$2\"://" | tr -d "\n\t" | sed -e 's/\\"/"/g' | sed -e 's/\\\\/\\/g' | sed -e 's/^[ \t]*//g' | sed -e 's/^"//'  -e 's/"$//'	
+#}
 
-json='{"code":200,"lang":"pt-en","text":["Hello"]}'
+#json='{"code":200,"lang":"pt-en","text":["Hello"]}'
 
-parse_json $json code
-parse_json $json text
+#parse_json $json code
+#parse_json $json text
+
+#echo '{"code":200,"lang":"pt-en","text":["Hello"]}' | jshon -e code 
+
+echo '{"code":200,"lang":"pt-en","text":["Hello"]}' | jshon -e text -e 0
--- falador/tradutor.sh
+++ falador/tradutor.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Declraçao de funções
-function parse_json()
-{	
-    echo $1 | sed -e 's/[{}]/''/g' | awk -F=':' -v RS=',' "\$1~/\"$2\"/ {print}" | sed -e "s/\"$2\"://" | tr -d "\n\t" | sed -e 's/\\"/"/g' | sed -e 's/\\\\/\\/g' | sed -e 's/^[ \t]*//g' | sed -e 's/^"//'  -e 's/"$//'	
-}
-
 # pega o conteúdo do texto passado como parâmetro
 texto=$@
 
@@ -20,16 +14,25 @@ API_KEY="trnsl.1.1.20141209T234029Z.5803500de6528911.910b9783819255adca607af43de
 # Defina a linguagem para tradução - https://api.yandex.com/translate/doc/dg/reference/getLangs.xml
 lang="pt-en"
 
-traducao=$(curl -A "Tradutor" "https://translate.yandex.net/api/v1.5/tr.json/translate?key=$API_KEY&lang=$lang&text=$texto")
+traducao=$(curl -s -A "Tradutor" "https://translate.yandex.net/api/v1.5/tr.json/translate?key=$API_KEY&lang=$lang&text=$texto")
 
-parse_json $traducao code
+# Parse JSON
+code=$(echo $traducao | jshon -e code)
 
-# Linguagem para fala
-tl="pt-BR"
+if [ "$code" == 200 ]; then
+	# Linguagem para fala
+	tl="en-us"
 
-# Texto para fala
-q="Oi"
+	# Texto para fala
+	text=$(echo $traducao | jshon -e text -e 0)		
 
-curl -A "Falador" "http://translate.google.com/translate_tts?ie=UTF-8&q=$q&tl=$tl" | mpg123 -;
+	echo "Tradução -> $text"
+
+	q=$text
+	curl -s -A "Falador" translate\.google\.com/translate_tts -d "tl=$tl&ie=UTF-8&q=$q" | mpg123 -q - ;
+else
+	message=$(echo $traducao | jshon -e message)
+	echo "Falha ao realizar a tradução do texto $message" 
+fi
 
 
